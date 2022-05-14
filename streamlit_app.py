b18f8bf89ce51393266f876ed707735d5f978664
sebastiandres <sebastiandres@gmail.com>
2022-05-13 23:43:15 -0400

    cleaner

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,18 @@ import pandas as pd
 st.title("SlowAPI")
 
 query_params = st.experimental_get_query_params()
-#st.write(query_params)
-
-if "user" not in st.session_state:
-    st.session_state.user = 1
 
 if "method" in query_params:
     if query_params["method"][0]=="post":
-        #st.write(query_params)
         user = query_params["user"][0]
         st.write(f"post user {user}")
         with open("file.txt", "a") as fh:
-            fh.write(f"post user {user}\n")
-        st.write(f"Wrote to file")
-        st.session_state.user += 1
+            fh.write(f"{user}\n")
+        st.write(f"Wrote user to file")
         st.experimental_set_query_params()
 else:
     st.write("Just the regular website")
-    #st.write(query_params)
+
+if st.button("Show File"):
+    with open("file.txt", "r") as fh:
+        st.write("\n".join(fh.readlines()))
